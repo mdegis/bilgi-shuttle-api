cffff214a589306dd6b3799dffe504885fedf09f
Altay Aydemir <altayaydemir@gmail.com>
2016-01-24 17:11:50 +0200

    Working API

--- shuttle/local_settings.py
+++ shuttle/local_settings.py
@@ -47,11 +47,13 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'respite',
     'shuttle',
+    'corsheaders',
 )
 
 #respite ile baslayan son uc eklendi
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -99,3 +101,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
+
+# Cors
+CORS_ORIGIN_ALLOW_ALL = True
